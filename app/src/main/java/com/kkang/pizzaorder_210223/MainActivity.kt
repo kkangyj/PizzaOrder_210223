2f7e149230a1a5b7f2d68d0974a3951510de3ab3
kkangyun <kkang_yj36@naver.com>
2021-03-03 15:04:29 +0900

    Initial commit

---
+++ app/src/main/java/com/kkang/pizzaorder_210223/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kkang.pizzaorder_210223
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
